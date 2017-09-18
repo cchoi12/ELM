9b39ebd789dd78564ae8c80de37520666944dc05
Christopher Choi <cdddchris@gmail.com>
2017-09-17 20:43:43 -0700

    working with html in elm

--- egghead/Main.elm
+++ egghead/Main.elm
@@ -2,19 +2,25 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+
 politely : String -> String
 politely phrase =
-  "Excuse me, " ++ phrase
+    "Excuse me, " ++ phrase
 
-ask : String ->  String -> String
+
+ask : String -> String -> String
 ask thing place =
-  "is there a "
-    ++ thing
-    ++ " in the "
-    ++ place
+    "is there a "
+        ++ thing
+        ++ " in the "
+        ++ place
+
 
 askPolitelyAboutFish : String -> String
-askPolitelyAboutFish = politely << (ask "fish")
+askPolitelyAboutFish =
+    politely << ask "fish"
+
+
 main =
-  text
-    <| askPolitelyAboutFish "hat"
+    text <|
+        askPolitelyAboutFish "hat"
